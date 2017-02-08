8a4ef83df80159e72d91869237e626fb9ae59624
Adam Butler <adam@lab.io>
2017-02-08 17:35:40 +0000

    Refactor navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,39 +1,33 @@
-IGNORED_FILES = ['.DS_Store']
+IGNORED_PATHS = ['..', '.', '.DS_Store']
 NAVIGATION_WEIGHT = YAML.load_file("#{Rails.root}/config/navigation.yml")["navigation_weight"]
 FLATTEN_TREES = ['Messaging']
 
 module ApplicationHelper
   def directory_hash(path, name=nil)
     data = { title: (name || path), path: path }
-    data[:children] = children = []
+    data[:children] = []
     Dir.foreach(path) do |entry|
-      next if (entry == '..' || entry == '.')
+      next if IGNORED_PATHS.include? entry
       full_path = File.join(path, entry)
       if File.directory?(full_path)
-        children << directory_hash(full_path, entry)
+        data[:children] << directory_hash(full_path, entry)
       else
-        children << { title: entry, path: full_path, is_file?: true }
+        data[:children] << { title: entry, path: full_path, is_file?: true }
       end
     end
-    return data
+
+    sort_navigation(data)
   end
 
-  def clean(context)
-    context.reject do |item|
-      IGNORED_FILES.include? item[:title]
-    end
-  end
-
-  def sort_by_directory_and_weight(context)
-    context.sort_by do |item|
+  def sort_navigation(context)
+    context[:children].sort_by! do |item|
       sort_array = []
-
       sort_array << (NAVIGATION_WEIGHT[normalised_title(item)] || 1000)
       sort_array << (item[:is_file?] ? 1 : 0)
       sort_array << (item[:is_file?] && document_meta(item[:path])["navigation_weight"] ? document_meta(item[:path])["navigation_weight"] : 1000)
-
       sort_array
     end
+    context
   end
 
   def path_to_url(path)
@@ -41,21 +35,14 @@ module ApplicationHelper
   end
 
   def first_link_in_directory(context)
-    context = clean(context)
-    context = sort_by_directory_and_weight(context)
-
     if context.any?
       if context.first[:is_file?]
         path_to_url(context.first[:path])
       else
         if context.first[:children]
           first_link_in_directory(context.first[:children])
-        else
-          '#'
         end
       end
-    else
-      '#'
     end
   end
 
@@ -66,9 +53,6 @@ module ApplicationHelper
   def directory(context = directory_hash("#{Rails.root}/_documentation")[:children], received_flatten = false)
     s = []
     s << '<ul>' unless received_flatten
-
-    context = clean(context)
-    context = sort_by_directory_and_weight(context)
 
     s << context.map do |child|
       ss = []
