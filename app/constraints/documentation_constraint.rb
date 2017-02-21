aab4dd13fa6c30f3e5f08a208df12e719323bd20
Adam Butler <adam@lab.io>
2017-02-21 12:39:15 +0000

    Fix route constraint

--- app/constraints/documentation_constraint.rb
+++ app/constraints/documentation_constraint.rb
@@ -2,6 +2,6 @@ class DocumentationConstraint
   attr_reader :product
 
   def matches?(request)
-    ['voice', 'messaging', 'account'].include? request.params[:product]
+    ['voice', 'messaging', 'account', 'global'].include? request.params[:product]
   end
 end
