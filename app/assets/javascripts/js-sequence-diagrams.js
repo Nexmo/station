3903ae762016f6362654bcaad18b685b9e7316b2
Adam Butler <adam@lab.io>
2017-02-28 17:42:34 +0000

    More UI tweaks

--- app/assets/javascripts/js-sequence-diagrams.js
+++ app/assets/javascripts/js-sequence-diagrams.js
@@ -2,4 +2,28 @@ $(document).on('turbolinks:load', function() {
   $(".diagram").sequenceDiagram({
     theme: 'simple'
   })
+
+  setTimeout(function(){
+    $('.diagram rect')
+      .attr('rx', 3)
+      .css('stroke-width', 2)
+      .attr('stroke', '#E4EEF3')
+
+    $('.diagram .actor text')
+      .attr('fill', '#E6547B')
+      .attr('font-family', 'Consolas, sans-serif')
+
+    $('.diagram')
+      .find('.note, .actor')
+      .attr('font-weight', 'bold')
+
+    $('.diagram marker')
+      .css('fill', '93B6C7')
+
+    $('.diagram .signal text')
+      .attr('fill', '#224E66')
+
+    $('.diagram line')
+      .attr('stroke', '#93B6C7')
+  }, 500)
 });
