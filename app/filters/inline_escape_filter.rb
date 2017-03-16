bd0a366108f6abec9bee4e20154f79c6cf152ac8
Adam Butler <adam@lab.io>
2017-03-16 17:19:24 +0000

    Remove log

--- app/filters/inline_escape_filter.rb
+++ app/filters/inline_escape_filter.rb
@@ -3,7 +3,6 @@ class InlineEscapeFilter < Banzai::Filter
     # Freeze to prevent Markdown formatting
     input.gsub(/``(.+?)``/) do |s|
       frozen_code = Base64.urlsafe_encode64("<code>#{$1}</code>")
-      Rails.logger.debug frozen_code
       "FREEZESTART#{frozen_code}FREEZEEND"
     end
   end
