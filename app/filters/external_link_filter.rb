025eabb2bf12a4015eae84f6859e8ffb82c844b8
Adam Butler <adam@lab.io>
2017-03-17 12:57:32 +0000

    Fix

--- app/filters/external_link_filter.rb
+++ app/filters/external_link_filter.rb
@@ -3,7 +3,7 @@ class ExternalLinkFilter < Banzai::Filter
     @input = input
 
     document.css('a').each do |link|
-      if link['href'].start_with? "http"
+      if link['href'] && link['href'].start_with?('http')
         link['target'] = '_blank'
         link.add_child <<~HEREDOC
           &nbsp;<i class="fa fa-external-link"></i>
