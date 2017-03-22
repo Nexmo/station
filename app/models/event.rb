9a74abda45a08a881d6d277b0c8f1242b531b4fd
Adam Butler <adam@lab.io>
2017-03-22 17:07:03 +0000

    Fix todayy -> today

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   default_scope -> { order(:starts_at) }
-  scope :upcoming, -> { where('starts_at > ?', Time.zone.todayy) }
+  scope :upcoming, -> { where('starts_at > ?', Time.zone.today) }
 
   def date_range
     starts = starts_at.strftime('%d %B %Y')
