3b12ec0390c1fea2b8f2c0e5bf48ec5a38eac67b
Adam Butler <adam@lab.io>
2017-04-11 15:04:37 +0100

    Disable search in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.file_watcher = ActiveSupport::FileUpdateChecker
+
+  config.search_enabled = true
 end
