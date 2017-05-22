7548d2ef91e0dec41f1a9a6915f72144dc22c541
Adam Butler <adam@lab.io>
2017-05-22 11:18:31 +0100

    Fix edgecase where freeze filter produces empty content

--- app/filters/unfreeze_filter.rb
+++ app/filters/unfreeze_filter.rb
@@ -1,7 +1,10 @@
 class UnfreezeFilter < Banzai::Filter
   def call(input)
-    input.gsub(/FREEZESTART(.+?)FREEZEEND/m) do |_s|
+    input.gsub!('FREEZESTARTFREEZEEND', '')
+
+    input.gsub!(/FREEZESTART(.+?)FREEZEEND/m) do |_s|
       Base64.urlsafe_decode64($1).force_encoding(Encoding::UTF_8)
     end
+    input
   end
 end
