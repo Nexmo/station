e4e994c1ff79d6c29682234294c452b8ca87a4c9
Adam Butler <adam@lab.io>
2017-08-02 12:27:18 +0100

    Swagger 2 -> Open API 3

--- spec/services/open_api/example_response_resolver_spec.rb
+++ spec/services/open_api/example_response_resolver_spec.rb
@@ -51,6 +51,99 @@ RSpec.describe OpenApi::ExampleResponseResolver do
     end
   end
 
+  context 'petstore-expanded' do
+    before(:each) do
+      @specification = OpenApiParser::Specification.resolve('spec/fixtures/open_api/petstore-expanded.yml')
+    end
+
+    it 'has a resolved path' do
+      resolver = OpenApi::ExampleResponseResolver.new(@specification, path: '/pets/{id}', method: 'get')
+      expect(resolver.resolved_path).to eq('/pets/1')
+    end
+
+    it 'has a status' do
+      resolver = OpenApi::ExampleResponseResolver.new(@specification, path: '/pets/{id}', method: 'get')
+      expect(resolver.status).to eq('200')
+    end
+
+
+    describe 'GET /pets' do
+      it 'provides an example response for an object' do
+        resolver = OpenApi::ExampleResponseResolver.new(@specification, path: '/pets', method: 'get')
+        expect(resolver.model.length).to eq(1)
+        expect(resolver.model[0]['id']).to eq(1)
+        expect(resolver.model[0]['name']).to eq('abc123')
+        expect(resolver.model[0]['tag']).to eq('abc123')
+      end
+    end
+
+    describe 'POST /pets' do
+      it 'provides an example response for an object' do
+        resolver = OpenApi::ExampleResponseResolver.new(@specification, path: '/pets', method: 'post')
+        expect(resolver.model['id']).to eq(1)
+      end
+    end
+
+    describe 'GET /pets/{id}' do
+      it 'provides an example response for an object' do
+        resolver = OpenApi::ExampleResponseResolver.new(@specification, path: '/pets/{id}', method: 'get')
+        expect(resolver.model['id']).to eq(1)
+        expect(resolver.model['name']).to eq('abc123')
+        expect(resolver.model['tag']).to eq('abc123')
+      end
+    end
+
+    describe 'DELETE /pets/{id}' do
+      it 'provides an example response for an object' do
+        resolver = OpenApi::ExampleResponseResolver.new(@specification, path: '/pets/{id}', method: 'delete')
+        expect(resolver.model).to be_nil
+      end
+    end
+  end
+
+  context 'petstore-jwt' do
+    before(:each) do
+      @specification = OpenApiParser::Specification.resolve('spec/fixtures/open_api/petstore-jwt.yml')
+    end
+
+    it 'has a resolved path' do
+      resolver = OpenApi::ExampleResponseResolver.new(@specification, path: '/pets', method: 'get')
+      expect(resolver.endpoint.raw['security']).to include('jwt')
+    end
+  end
+
+  context 'uber' do
+    before(:each) do
+      @specification = OpenApiParser::Specification.resolve('_open_api/example/uber.yml')
+    end
+
+    it 'has a resolved path' do
+      resolver = OpenApi::ExampleResponseResolver.new(@specification, path: '/products', method: 'get')
+      expect(resolver.resolved_path).to eq('/products')
+    end
+
+    describe 'GET /products' do
+      it 'provides an example response for an object' do
+        resolver = OpenApi::ExampleResponseResolver.new(@specification, path: '/products', method: 'get')
+        expect(resolver.model.length).to eq(1)
+      end
+    end
+
+    describe 'GET /history' do
+      it 'provides an example response for an object' do
+        resolver = OpenApi::ExampleResponseResolver.new(@specification, path: '/history', method: 'get')
+        expect(resolver.model.length).to eq(4)
+      end
+    end
+
+    describe 'GET /me' do
+      it 'provides an example response for an object' do
+        resolver = OpenApi::ExampleResponseResolver.new(@specification, path: '/me', method: 'get')
+        expect(resolver.model.length).to eq(5)
+      end
+    end
+  end
+
   context 'sms' do
     before(:each) do
       @specification = OpenApiParser::Specification.resolve('spec/fixtures/open_api/sms.yml')
