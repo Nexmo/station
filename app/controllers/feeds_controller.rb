b666e92fe9466e69cac388906adc92fa139e19ea
Adam Butler <adam@lab.io>
2017-08-24 15:33:51 +0100

    Add link, move controller

---
+++ app/controllers/feeds_controller.rb
@@ -0,0 +1,17 @@
+class FeedsController < ApplicationController
+  def events
+    calendar = Icalendar::Calendar.new
+
+    Event.all.each do |event|
+      calendar.event do |e|
+        e.dtstart = Icalendar::Values::Date.new(event.starts_at)
+        e.dtend = Icalendar::Values::Date.new(event.ends_at + 1.day)
+        e.summary = event.title
+        e.description = event.description
+      end
+    end
+
+    calendar.publish
+    render plain: calendar.to_ical
+  end
+end
