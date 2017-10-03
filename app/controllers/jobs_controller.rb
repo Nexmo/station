02f40847a926d046314ec6e20cb0726b9be3a69a
Adam Butler <adam@lab.io>
2017-10-03 15:25:03 +0100

    Verify GitHub signature

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,23 +2,27 @@ class JobsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def code_example_push
-    url = "https://api.travis-ci.org/repo/#{ENV['TRAVIS_REPO_ID']}/requests"
+    if validate_github_signature
+      url = "https://api.travis-ci.org/repo/#{ENV['TRAVIS_REPO_ID']}/requests"
 
-    RestClient.post(url, {
-      "request": {
-        "branch": 'ci/diff',
-        "config": {
-          "script": 'rake diff:execute',
+      RestClient.post(url, {
+        "request": {
+          "branch": 'ci/diff',
+          "config": {
+            "script": 'rake diff:execute',
+          },
         },
-      },
-    }.to_json, {
-      content_type: :json,
-      accept: :json,
-      'Authorization': "token #{ENV['TRAVIS_TOKEN']}",
-      'Travis-API-Version': '3',
-    })
+      }.to_json, {
+        content_type: :json,
+        accept: :json,
+        'Authorization': "token #{ENV['TRAVIS_TOKEN']}",
+        'Travis-API-Version': '3',
+      })
 
-    head :ok
+      head :ok
+    else
+      head :unauthorized
+    end
   end
 
   def open_pull_request
@@ -33,5 +37,12 @@ class JobsController < ApplicationController
     client.create_pull_request(repo, branch_base, branch_head, title, body)
 
     head :ok
+  end
+
+  private
+
+  def validate_github_signature
+    signature = "sha1=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['GITHUB_SIGNATURE'], request.raw_post)}"
+    Rack::Utils.secure_compare(signature, request.headers['X-Hub-Signature'])
   end
 end
