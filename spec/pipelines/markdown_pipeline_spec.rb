663e499f4cd3421525159fda4bdb03fed0220451
Adam Butler <adam@lab.io>
2017-10-23 17:53:57 +0100

    Spec for with and without <?php tag

--- spec/pipelines/markdown_pipeline_spec.rb
+++ spec/pipelines/markdown_pipeline_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 RSpec.describe MarkdownPipeline do
   context 'inline code examples' do
+    it 'highlights PHP examples that begin with <?php' do
+      input = <<~HEREDOC
+        ```php
+        <?php
+
+        echo '<p>Hello World</p>';
+        ```
+      HEREDOC
+
+      output = MarkdownPipeline.new.call(input)
+
+      expect(output).to include('<pre class="highlight php"><code><span class="o">&lt;?</span>')
+    end
+
     it 'highlights PHP examples that do not begin with <?php' do
       input = <<~HEREDOC
         ```php
@@ -16,10 +30,21 @@ RSpec.describe MarkdownPipeline do
   end
 
   context 'injected code examples' do
+    it 'highlights PHP examples that begin with <?php' do
+      input = <<~HEREDOC
+        ```tabbed_examples
+        source: '/spec/fixtures/filters/tabbed_examples_php_tagged'
+        ```
+      HEREDOC
+
+      output = MarkdownPipeline.new.call(input)
+      expect(output).to include('<span class="nv">')
+    end
+
     it 'highlights PHP examples that do not begin with <?php' do
       input = <<~HEREDOC
         ```tabbed_examples
-        source: '/spec/fixtures/filters/tabbed_examples'
+        source: '/spec/fixtures/filters/tabbed_examples_php_untagged'
         ```
       HEREDOC
 
