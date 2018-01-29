9b98cc183b80c7357a537e6e192ddd50bf1cabb0
Adam Butler <adam@lab.io>
2018-01-29 15:22:30 +0000

    Isolate NI from OAS changes

--- _examples/curl/number_insight/async.sh
+++
@@ -1,5 +0,0 @@
-curl https://api.nexmo.com/ni/standard/async/json \
--d 'api_key=NEXMO_API_KEY' \
--d 'api_secret=NEXMO_API_SECRET' \
--d 'number=447700900000' \
--d 'callback=https://example.com/webhooks/event'
