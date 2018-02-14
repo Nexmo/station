b9c5d0d3af4cf39162b232191dc5bb083244cffa
Tom Morris <tom@tommorris.org>
2018-02-14 15:54:55 +0000

    Fix IconFilter regex
    Previous regex was throwing an error as the square bracket was unescaped
    
    Signed-off-by: Tom Morris <tom@tommorris.org>

--- app/filters/icon_filter.rb
+++ app/filters/icon_filter.rb
@@ -3,7 +3,7 @@ class IconFilter < Banzai::Filter
     input.gsub!('✅', '<i class="icon icon--large icon-check-circle color--success"></i>')
     input.gsub!('❎', '<i class="icon icon--large icon-times-circle color--error"></i>')
 
-    input.gsub!(/\[icon="(.+?)"]/) do
+    input.gsub!(/\[icon="(.+?)"\]/) do
       <<~HEREDOC
         <i class="icon icon-#{$1}"></i>
       HEREDOC
