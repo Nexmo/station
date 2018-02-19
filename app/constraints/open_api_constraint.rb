56b890d9a077180ab8dc8836926736e2d4603e8c
Adam Butler <adam@lab.io>
2018-02-19 12:51:42 +0000

    Fix for #583

---
+++ app/constraints/open_api_constraint.rb
@@ -0,0 +1,11 @@
+OPEN_API_PRODUCTS = %w(
+  sms
+  media
+  number-insight
+)
+
+class OpenApiConstraint
+  def self.products
+    { definition: Regexp.new(OPEN_API_PRODUCTS.join('|')) }
+  end
+end
