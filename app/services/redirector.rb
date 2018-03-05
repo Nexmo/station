05a794218e4cf3d577512996979a7ea1980c2624
Adam Butler <adam@lab.io>
2018-03-05 16:32:45 +0000

    Add environment redirect support

--- app/services/redirector.rb
+++ app/services/redirector.rb
@@ -2,6 +2,18 @@ REDIRECTS = YAML.load_file("#{Rails.root}/config/redirects.yml")
 
 class Redirector
   def self.find(request)
+    find_by_config(request) || find_by_enviornment_redirect(request)
+  end
+
+  def self.find_by_config(request)
     REDIRECTS[request.path] || false
+  end
+
+  def self.find_by_enviornment_redirect(request)
+    return false unless ENV['ENVIRONMENT_REDIRECTS']
+    ENV['ENVIRONMENT_REDIRECTS'].each do |path, new_url|
+      /#{Regexp.quote(path)}/ =~ request.path
+      break new_url
+    end
   end
 end
--- spec/services/redirector_spec.rb
+++ spec/services/redirector_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Redirector do
     request = OpenStruct.new(path: '/sms')
     expect(Redirector.find(request)).to eq('/messaging/sms/overview')
   end
+
+  context 'when a URL is provided in the environment' do
+    before do
+      allow(ENV).to receive(:[]).with('ENVIRONMENT_REDIRECTS').and_return({
+        '^\/foo\/bar\.*?': 'https://google.com',
+      })
+    end
+
+    it 'returns a redirected path' do
+      request = OpenStruct.new(path: '/foo/bar')
+      expect(Redirector.find(request)).to eq('https://google.com')
+    end
+  end
 end
