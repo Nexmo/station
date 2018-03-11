10a8e24f64110e4cd48b275f060ad28742432ba7
Hugh Hopkins <hhopkins@VONC1MSN0ARHF4F.local>
2018-03-11 08:22:20 -0700

    update curl WF example

--- _examples/curl/olympus/workflows/send-failover-sms-viber.sh
+++ _examples/curl/olympus/workflows/send-failover-sms-viber.sh
@@ -1,30 +1,33 @@
-curl -X "POST" "https://api.nexmo.com/beta/workflows" \
-  -H "Authorization: Bearer "$JWT\
+curl -X POST https://api.nexmo.com/beta/workflows \
+  -H 'Authorization: Bearer $JWT'\
   -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
-  -d $'[
-    {
-      "from": { "type": "viber_service_msg", "id": "VIBER_SERVICE_MESSAGE_ID" }
-      "to": { "type": "viber_service_msg", "number": "TO_NUMBER" },
-      "message": {
-        "content": {
-          "type": "text",
-          "text": "This is a Viber Service Message sent from the Messages API"
+  -d $'{
+    "template":"failover",
+    "workflow": [
+      {
+        "from": { "type": "viber_service_msg", "id": "VIBER_SERVICE_MESSAGE_ID"},
+        "to": { "type": "viber_service_msg", "number": "TO_NUMBER"},
+        "message": {
+          "content": {
+            "type": "text",
+            "text": "This is a Viber Service Message sent from the Messages API"
+          }
+        },
+        "failover":{
+          "expiry_time": 600,
+          "condition_status": "delivered"
         }
       },
-      "failover": {
-        "condition_status": "delivered",
-        "expiry_time": 600
-      },
-    },
-    {
-      "to": { "type": "sms", "number": "TO_NUMBER" },
-      "from": { "type": "sms", "number": "FROM_NUMBER" }
-      "message": {
-        "content": {
-          "type": "text",
-          "text": "This is an SMS sent from the Messages API"
+      {
+        "from": {"type": "sms", "number": "FROM_NUMBER"},
+        "to": { "type": "sms", "number": "TO_NUMBER"},
+        "message": {
+          "content": {
+            "type": "text",
+            "text": "This is an SMS sent from the Messages API"
+          }
         }
-      },
-    }
-  ]'
+      }
+    ]
+  }'
