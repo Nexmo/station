abd66bf0d7cfe350a171281d739ba84abfc43013
Chris Guzman <guzmantchris@gmail.com>
2018-05-30 08:27:27 -0500

    Stitch Android 1.4 iOS 1.5

--- .repos/nexmo/conversation-docs/initialize/application.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$keypair = new \Nexmo\Client\Credentials\Keypair(
-    file_get_contents(NEXMO_APPLICATION_PRIVATE_KEY_PATH),
-    NEXMO_APPLICATION_ID
-);
-
-$client = new \Nexmo\Client($keypair);
