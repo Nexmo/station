6a30ed68f26f5bf8f3319211c22801c3d62e8ec7
benhayehudi <bengreenberg@gmail.com>
2018-12-26 09:56:19 +0200

    Added testing for PHPInlinerFilter

---
+++ spec/filters/php_inliner_filter_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe PHPInlinerFilter do
+  it 'adds "?start_inline=1" to regex matching input' do
+    input = <<~HEREDOC
+      ```php
+      $var = 'Hello World!'
+      ```
+    HEREDOC
+
+    expected_output = <<~HEREDOC
+      ```php?start_inline=1
+      $var = 'Hello World!'
+      ```
+    HEREDOC
+
+    expect(described_class.call(input)).to eql(expected_output)
+  end
+
+  it 'does not add "?start_inline=1" to input that does not match regex' do
+    input = <<~HEREDOC
+      ```ruby
+      var = 'Hello World!'
+      ```
+    HEREDOC
+
+    expected_output = <<~HEREDOC
+      ```ruby
+      var = 'Hello World!'
+      ```
+    HEREDOC
+
+    expect(described_class.call(input)).to eql(expected_output)
+  end
+
+  it 'does nothing to whitespace input' do
+    input = ' '
+
+    expected_output = ' '
+
+    expect(described_class.call(input)).to eql(expected_output)
+  end
+end
