92ef2e8be2f0e4df19bd4cece3fc5059a2b68757
Tim Craft <mail@timcraft.com>
2019-02-04 12:03:44 +0000

    Remove Enumerable#each_with_position method

--- lib/core_ext/enumerable.rb
+++
@@ -1,9 +0,0 @@
-module Enumerable
-  def each_with_position
-    each_with_index do |element, index|
-      first = index.zero?
-      last = index.equal?(size - 1)
-      yield(element, first, last)
-    end
-  end
-end
