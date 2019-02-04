92e3dfa56f95d04d644e91ff47847f514c9375de
Tim Craft <mail@timcraft.com>
2019-02-04 11:11:08 +0000

    Remove empty spec files

--- spec/controllers/markdown_controller_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe MarkdownController, type: :controller do
-end
--- spec/controllers/slack_controller_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SlackController, type: :controller do
-end
