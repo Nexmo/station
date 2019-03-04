db5f61cc435e2e97c056494f7f8bb88a2146e45f
Ben Greenberg <benhayehudi@users.noreply.github.com>
2019-03-04 13:58:27 +0200

    Add FreezeFilter support in ModalFilter (#1497)

--- spec/filters/modal_filter_spec.rb
+++ spec/filters/modal_filter_spec.rb
@@ -2,44 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ModalFilter do
   it 'takes input of title and markdown link and produces HTML content' do
+    allow(SecureRandom).to receive(:uuid).and_return('12345')
     input = '@[Possible values](/_modals/api/developer/message/search/response/final-status.md)'
 
-    expected_output = <<~HEREDOC
-      <div class="Vlt-table Vlt-table--data Vlt-table--bordered"><table>
-      <thead>
-      <tr>
-      <th>Value</th>
-      <th>Description</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-      <td><code>DELIVRD</code></td>
-      <td>This message has been delivered to the phone number.</td>
-      </tr>
-      <tr>
-      <td><code>EXPIRED</code></td>
-      <td>The target carrier did not send a status in the 48 hours after this message was delivered to them.</td>
-      </tr>
-      <tr>
-      <td><code>UNDELIV</code></td>
-      <td>The target carrier failed to deliver this message.</td>
-      </tr>
-      <tr>
-      <td><code>REJECTD</code></td>
-      <td>The target carrier rejected this message.</td>
-      </tr>
-      <tr>
-      <td><code>UNKNOWN</code></td>
-      <td>The target carrier has returned an undocumented status code.</td>
-      </tr>
-      </tbody>
-      </table></div>
-      </div>
-    HEREDOC
+    expected_output = "<a data-open='12345'>Possible values</a>FREEZESTARTPGRpdiBjbGFzcz0icmV2ZWFsIiBpZD0iMTIzNDUiIGRhdGEtcmV2ZWFsPgogIDxkaXYgY2xhc3M9IlZsdC10YWJsZSBWbHQtdGFibGUtLWRhdGEgVmx0LXRhYmxlLS1ib3JkZXJlZCI-PHRhYmxlPgo8dGhlYWQ-Cjx0cj4KPHRoPlZhbHVlPC90aD4KPHRoPkRlc2NyaXB0aW9uPC90aD4KPC90cj4KPC90aGVhZD4KPHRib2R5Pgo8dHI-Cjx0ZD48Y29kZT5ERUxJVlJEPC9jb2RlPjwvdGQ-Cjx0ZD5UaGlzIG1lc3NhZ2UgaGFzIGJlZW4gZGVsaXZlcmVkIHRvIHRoZSBwaG9uZSBudW1iZXIuPC90ZD4KPC90cj4KPHRyPgo8dGQ-PGNvZGU-RVhQSVJFRDwvY29kZT48L3RkPgo8dGQ-VGhlIHRhcmdldCBjYXJyaWVyIGRpZCBub3Qgc2VuZCBhIHN0YXR1cyBpbiB0aGUgNDggaG91cnMgYWZ0ZXIgdGhpcyBtZXNzYWdlIHdhcyBkZWxpdmVyZWQgdG8gdGhlbS48L3RkPgo8L3RyPgo8dHI-Cjx0ZD48Y29kZT5VTkRFTElWPC9jb2RlPjwvdGQ-Cjx0ZD5UaGUgdGFyZ2V0IGNhcnJpZXIgZmFpbGVkIHRvIGRlbGl2ZXIgdGhpcyBtZXNzYWdlLjwvdGQ-CjwvdHI-Cjx0cj4KPHRkPjxjb2RlPlJFSkVDVEQ8L2NvZGU-PC90ZD4KPHRkPlRoZSB0YXJnZXQgY2FycmllciByZWplY3RlZCB0aGlzIG1lc3NhZ2UuPC90ZD4KPC90cj4KPHRyPgo8dGQ-PGNvZGU-VU5LTk9XTjwvY29kZT48L3RkPgo8dGQ-VGhlIHRhcmdldCBjYXJyaWVyIGhhcyByZXR1cm5lZCBhbiB1bmRvY3VtZW50ZWQgc3RhdHVzIGNvZGUuPC90ZD4KPC90cj4KPC90Ym9keT4KPC90YWJsZT48L2Rpdj4KPC9kaXY-Cg==FREEZEEND"
 
-    # using .include because the <a data-open='..'> tag changes each time it is executed
-    expect(described_class.call(input)).to include(expected_output)
+    expect(described_class.call(input)).to eq(expected_output)
   end
 
   it 'does not transform text that does not match the regex' do
