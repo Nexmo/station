6e0a9ce9dc8c468a54856e303f978058d3c8f3da
Michael Heap <m@michaelheap.com>
2019-04-11 12:50:44 +0100

    Remove Flatten from Navigation generation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ IGNORED_PATHS = ['..', '.', '.DS_Store'].freeze
 NAVIGATION = YAML.load_file("#{Rails.root}/config/navigation.yml")
 NAVIGATION_WEIGHT = NAVIGATION['navigation_weight']
 NAVIGATION_OVERRIDES = NAVIGATION['navigation_overrides']
-FLATTEN_TREES = [].freeze
 COLLAPSIBLE = ['Messages API', 'Dispatch API', 'Messaging', 'SMS', 'Conversion API', 'SNS', 'US Short Codes', 'Voice', 'Number Insight', 'Account', 'Global', 'SIP', 'Voice API'].freeze
 
 # What tasks do we have available?
@@ -132,16 +131,14 @@ module ApplicationHelper
     directory(context, true, false, active_path)
   end
 
-  def directory(context = directory_hash("#{Rails.root}/_documentation")[:children], root = true, received_flatten = false, active_path = nil)
+  def directory(context = directory_hash("#{Rails.root}/_documentation")[:children], root = true, active_path = nil)
     active_path ||= request.path
 
     s = []
-    unless received_flatten
-      namespace = params[:namespace].presence || 'documentation'
-      s << (root ? "<ul class='Vlt-sidemenu Vlt-sidemenu--rounded navigation js-navigation navigation--#{namespace}'>" : '<ul class="Vlt-sidemenu__list--compressed">')
-    end
+    namespace = params[:namespace].presence || 'documentation'
+    s << (root ? "<ul class='Vlt-sidemenu Vlt-sidemenu--rounded navigation js-navigation navigation--#{namespace}'>" : '<ul class="Vlt-sidemenu__list--compressed">')
+
     s << context.map do |child|
-      flatten = FLATTEN_TREES.include? normalised_title(child)
       class_name = (COLLAPSIBLE.include? normalised_title(child)) ? 'js--collapsible' : ''
       configuration_identifier = url_to_configuration_identifier(path_to_url(child[:path]))
       options = configuration_identifier.split('.').inject(NAVIGATION_OVERRIDES) { |h, k| h[k] || {} }
@@ -152,57 +149,55 @@ module ApplicationHelper
       end
 
       ss = []
-      ss << "<li class='#{class_name} navigation-item'>" unless received_flatten
+      ss << "<li class='#{class_name} navigation-item'>"
 
-      unless flatten
-        url = (child[:is_file?] ? path_to_url(child[:path]) : first_link_in_directory(child[:children]))
-        has_active_class = (active_path == url) || active_path.start_with?("#{url}/")
+      url = (child[:is_file?] ? path_to_url(child[:path]) : first_link_in_directory(child[:children]))
+      has_active_class = (active_path == url) || active_path.start_with?("#{url}/")
 
-        # Handle tutorials
-        if @navigation == :tutorials
-          has_active_class = url == "/#{@product}/tutorials"
-        end
-
-        if !child[:is_file?]
-          if context.first[:children]
-            ss << "<a class='Vlt-sidemenu__trigger'>"
-
-            if options['svg'] && options['svgColor'] # rubocop:disable Metrics/BlockNesting
-              ss << '<svg class="Vlt-' + options['svgColor'] + '"><use xlink:href="/symbol/volta-icons.svg#Vlt-icon-' + options['svg'] + '" /></svg>'
-            end
-
-            if options['label'] # rubocop:disable Metrics/BlockNesting
-              additional_classes = ' '
-              additional_classes += 'Vlt-bg-green' if options['label'].casecmp('beta').zero? # rubocop:disable Metrics/BlockNesting
-              ss << '<span class="Vlt-sidemenu__label">'.html_safe + (normalised_title(child) + content_tag(:span, options['label'], class: 'Vlt-badge Vlt-badge--margin-left' + additional_classes)).html_safe + '</span>'.html_safe
-            else
-              ss << "<span class='Vlt-sidemenu__label'>#{normalised_title(child)}</span>"
-            end
-            ss << '</a>'
-          else
-            ss << "<h5 class='Vlt-sidemenu__title Vlt-sidemenu__title--border'>#{normalised_title(child)}</h5>"
-          end
-        elsif options['link'] == false
-          ss << "<span>#{normalised_title(child)}</span>"
-        else
-          link = link_to url, class: "#{has_active_class ? 'Vlt-sidemenu__link Vlt-sidemenu__link_active' : 'Vlt-sidemenu__link'}" do
-            if options['label']
-              additional_classes = ' '
-              additional_classes += 'Vlt-bg-green' if options['label'].casecmp('beta').zero? # rubocop:disable Metrics/BlockNesting
-              '<span class="Vlt-sidemenu__label">'.html_safe + (normalised_title(child) + content_tag(:span, options['label'], class: 'Vlt-badge Vlt-badge--margin-left' + additional_classes)).html_safe + '</span>'.html_safe
-            elsif options['svg']
-              ('<svg class="Vlt-' + options['svgColor'] + '"><use xlink:href="/symbol/volta-icons.svg#Vlt-icon-' + options['svg'] + '" /></svg><span class="Vlt-sidemenu__label">').html_safe + normalised_title(child) + '</span>'.html_safe
-            else
-              normalised_title(child)
-            end
-          end
-
-          ss << link
-        end
+      # Handle tutorials
+      if @navigation == :tutorials
+        has_active_class = url == "/#{@product}/tutorials"
       end
 
-      ss << directory(child[:children], false, flatten, active_path) if child[:children]
-      ss << '</li>' unless received_flatten
+      if !child[:is_file?]
+        if context.first[:children]
+          ss << "<a class='Vlt-sidemenu__trigger'>"
+
+          if options['svg'] && options['svgColor']
+            ss << '<svg class="Vlt-' + options['svgColor'] + '"><use xlink:href="/symbol/volta-icons.svg#Vlt-icon-' + options['svg'] + '" /></svg>'
+          end
+
+          if options['label']
+            additional_classes = ' '
+            additional_classes += 'Vlt-bg-green' if options['label'].casecmp('beta').zero? # rubocop:disable Metrics/BlockNesting
+            ss << '<span class="Vlt-sidemenu__label">'.html_safe + (normalised_title(child) + content_tag(:span, options['label'], class: 'Vlt-badge Vlt-badge--margin-left' + additional_classes)).html_safe + '</span>'.html_safe
+          else
+            ss << "<span class='Vlt-sidemenu__label'>#{normalised_title(child)}</span>"
+          end
+          ss << '</a>'
+        else
+          ss << "<h5 class='Vlt-sidemenu__title Vlt-sidemenu__title--border'>#{normalised_title(child)}</h5>"
+        end
+      elsif options['link'] == false
+        ss << "<span>#{normalised_title(child)}</span>"
+      else
+        link = link_to url, class: "#{has_active_class ? 'Vlt-sidemenu__link Vlt-sidemenu__link_active' : 'Vlt-sidemenu__link'}" do
+          if options['label']
+            additional_classes = ' '
+            additional_classes += 'Vlt-bg-green' if options['label'].casecmp('beta').zero?
+            '<span class="Vlt-sidemenu__label">'.html_safe + (normalised_title(child) + content_tag(:span, options['label'], class: 'Vlt-badge Vlt-badge--margin-left' + additional_classes)).html_safe + '</span>'.html_safe
+          elsif options['svg']
+            ('<svg class="Vlt-' + options['svgColor'] + '"><use xlink:href="/symbol/volta-icons.svg#Vlt-icon-' + options['svg'] + '" /></svg><span class="Vlt-sidemenu__label">').html_safe + normalised_title(child) + '</span>'.html_safe
+          else
+            normalised_title(child)
+          end
+        end
+
+        ss << link
+      end
+
+      ss << directory(child[:children], false, active_path) if child[:children]
+      ss << '</li>'
       ss.join("\n")
     end
 
@@ -215,7 +210,7 @@ module ApplicationHelper
       end
     end
 
-    s << '</ul>' unless received_flatten
+    s << '</ul>'
 
     s.join("\n").html_safe
   end
