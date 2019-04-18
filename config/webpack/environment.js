cdc47245f3b679222389ff19517f03dde11b145a
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2019-04-18 10:00:12 +0200

    Update .babelrc and splitChunk configuration.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,22 +4,6 @@ const { environment } = require('@rails/webpacker')
 const babelLoader = environment.loaders.get('babel')
 babelLoader.exclude = []
 
-environment.plugins.append(
-  'CommonsChunkCommons',
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'commons',
-    minChunks: (module) => {
-      return module.context && module.context.indexOf('node_modules') !== -1
-    },
-  })
-)
-
-environment.plugins.append(
-  'CommonsChunkManifest',
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'manifest',
-    minChunks: Infinity,
-  })
-)
+environment.splitChunks()
 
 module.exports = environment
