67de9cb041cd6380ea9e6936cad24dc793d799aa
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2019-07-08 14:28:55 +0200

    Reset Spotlight form after submit.

--- app/javascript/spotlight/index.js
+++ app/javascript/spotlight/index.js
@@ -4,6 +4,7 @@ export default () => {
 
     $(document).on('ajax:success', '#spotlight-form', function(event, response) {
       Volta.flash.show('success');
+      event.target.reset();
     }).on('ajax:error', '#spotlight-form', function(event) {
       Volta.flash.show('error');
     });
