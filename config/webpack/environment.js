7a4e197f44c6b78d75674e292b040047ff8385aa
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2019-09-02 16:24:18 +0200

    Initial commit.
    * Add vue-js config
    * Add concatenation component

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,13 @@
 const webpack = require('webpack')
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
 const babelLoader = environment.loaders.get('babel')
 babelLoader.exclude = []
 
 environment.splitChunks()
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
