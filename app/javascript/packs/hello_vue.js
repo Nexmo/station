5d334f0812544ca3e62c5a81186d44d42449fe4f
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2019-09-19 11:25:32 +0200

    Rename <Generator> to <JwtGenerator>.

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,9 +1,9 @@
 import Vue from 'vue/dist/vue.esm'
-import Generator from '../components/jwt_generator/generator.vue'
+import JwtGenerator from '../components/jwt_generator/JwtGenerator.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#hello',
-    components: { Generator }
+    components: { JwtGenerator }
   })
 })
