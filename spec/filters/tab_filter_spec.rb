7cb0bac9fce901ab589479b01d1790dfac1a07ed
benhayehudi <bengreenberg@gmail.com>
2019-09-23 14:58:15 +0300

    began adding tests

--- spec/filters/tab_filter_spec.rb
+++ spec/filters/tab_filter_spec.rb
@@ -27,4 +27,28 @@ RSpec.describe TabFilter do
       end.to raise_error('Source or tabs must be present in this tabbed_example config')
     end
   end
+
+  context 'when input is a directory' do
+    it 'raises an exception if there is no .config.yml file' do
+      expect(File).to receive(:directory?).and_return(true)
+      expect(File).to receive(:exist?).and_return(false)
+      input = 'nil'
+      expect do
+        described_class.new.call(input)
+      end.to raise_error('Tabbed must be set to true in the folder config YAML file')
+    end
+
+    it 'raises an exception if tabbed parameter is not set to true' do
+      expect(File).to receive(:read).with('/path/to/.config.yml').and_return(config_tabbed_false)
+      expect do
+        described_class.new.call(input)
+      end.to raise_error('Tabbed must be set to true in the folder config YAML file')
+    end
+  end
+
+  def config_tabbed_false
+    <<~HEREDOC
+      tabbed: false
+    HEREDOC
+  end
 end
