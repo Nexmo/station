f850edc979732904f07522c69a6b87410977a742
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2019-10-22 15:21:48 +0200

    Exclude Nokogiri's monkeypatch from zeitwerk.
    It was causing it to fail in production because of the folder structure
    and what was expected to be defined. See https://github.com/rails/rails/issues/36100#issuecomment-486927935
    for more info.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module NexmoDeveloper
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    Rails.autoloaders.main.ignore("#{Rails.root}/app/extensions")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
