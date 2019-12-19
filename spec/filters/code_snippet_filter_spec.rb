0e799634366f08fe1b0ac9c466e11503e82ad5c0
benhayehudi <bengreenberg@gmail.com>
2019-12-19 14:34:11 +0200

    snapshot test for first iteration of return possibility with methods to mock the data

--- spec/filters/code_snippet_filter_spec.rb
+++ spec/filters/code_snippet_filter_spec.rb
@@ -8,11 +8,28 @@ RSpec.describe CodeSnippetFilter do
   end
 
   it 'returns an error if no input provided' do
-    expect { described_class.call }.to raise_error(ArgumentError)  
+    expect { described_class.call }.to raise_error(ArgumentError)
   end
 
-  it "returns code_html if config['code_html'] is truthy" do
-    # snapshot test
+  it 'returns code snippet in the code only template if code only parameter is defined as true' do
+    expect(File).to receive(:exist?).and_return(true)
+    expect(File).to receive(:read).with(/example_snippet/).and_return(example_source_file)
+    expect(File).to receive(:read).with(/code_only/).and_return(code_only_template)
+
+    input = <<~HEREDOC
+      ```single_code_snippet
+      language: ruby
+      title: Ruby
+      code:
+        source: .repos/nexmo/nexmo-ruby-code-snippets/example/example_snippet.rb
+        from_line: 18
+        to_line: 32
+      unindent: false
+      code_only: true
+      ```
+    HEREDOC
+
+    expect(described_class.call(input)).to match_snapshot('code_snippet_filter/code_only_default')
   end
 
   it "returns prereqs + code_html + run_html if config['code_html'] is falsey" do
@@ -57,4 +74,52 @@ RSpec.describe CodeSnippetFilter do
 
   it 'reads write code snippet data if code only is not specified' do
   end
+end
+
+def example_source_file
+  <<~HEREDOC
+    require 'dotenv/load'
+    require 'nexmo'
+
+    NEXMO_API_KEY = ENV['NEXMO_API_KEY']
+    NEXMO_API_SECRET = ENV['NEXMO_API_SECRET']
+    NEXMO_APPLICATION_ID = ENV['NEXMO_APPLICATION_ID']
+    NEXMO_APPLICATION_PRIVATE_KEY_PATH = ENV['NEXMO_APPLICATION_PRIVATE_KEY_PATH']
+    NEXMO_NUMBER = ENV['NEXMO_NUMBER']
+    TO_NUMBER = ENV['TO_NUMBER']
+
+    client = Nexmo::Client.new(
+      api_key: NEXMO_API_KEY,
+      api_secret: NEXMO_API_SECRET,
+      application_id: NEXMO_APPLICATION_ID,
+      private_key: File.read(NEXMO_APPLICATION_PRIVATE_KEY_PATH)
+    )
+
+    response = client.calls.create(
+      to: [{
+        type: 'phone',
+        number: TO_NUMBER
+      }],
+      from: {
+        type: 'phone',
+        number: NEXMO_NUMBER
+      },
+      answer_url: [
+        'https://developer.nexmo.com/ncco/tts.json'
+      ]
+    )
+
+    puts response.inspect
+  HEREDOC
+end
+
+def code_only_template
+  <<~HEREDOC
+    <div class="copy-wrapper">
+      <div class="copy-button" data-lang="<%= lang %>" data-block="<%= config['source'] %>" data-section="code">
+        <%= octicon "clippy", :class => 'top left' %> <span><%= I18n.t('.copy-to-clipboad') %></span>
+      </div>
+      <pre class="highlight <%= lexer.tag %> main-code"><code><%= highlighted_code_source %></code></pre>
+    </div>
+  HEREDOC
 end
