c7ab61981b69ea479b9b3f8a33e7ae51b149531b
benhayehudi <bengreenberg@gmail.com>
2020-01-06 10:08:02 +0200

    moved helper method into describe

--- spec/filters/code_snippet_filter_spec.rb
+++ spec/filters/code_snippet_filter_spec.rb
@@ -1,6 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe CodeSnippetFilter do
+  def example_source_file
+    <<~HEREDOC
+      require 'dotenv/load'
+      require 'nexmo'
+      NEXMO_API_KEY = ENV['NEXMO_API_KEY']
+      NEXMO_API_SECRET = ENV['NEXMO_API_SECRET']
+      NEXMO_APPLICATION_ID = ENV['NEXMO_APPLICATION_ID']
+      NEXMO_APPLICATION_PRIVATE_KEY_PATH = ENV['NEXMO_APPLICATION_PRIVATE_KEY_PATH']
+      NEXMO_NUMBER = ENV['NEXMO_NUMBER']
+      TO_NUMBER = ENV['TO_NUMBER']
+      client = Nexmo::Client.new(
+        api_key: NEXMO_API_KEY,
+        api_secret: NEXMO_API_SECRET,
+        application_id: NEXMO_APPLICATION_ID,
+        private_key: File.read(NEXMO_APPLICATION_PRIVATE_KEY_PATH)
+      )
+      response = client.calls.create(
+        to: [{
+          type: 'phone',
+          number: TO_NUMBER
+        }],
+        from: {
+          type: 'phone',
+          number: NEXMO_NUMBER
+        },
+        answer_url: [
+          'https://developer.nexmo.com/ncco/tts.json'
+        ]
+      )
+      puts response.inspect
+    HEREDOC
+  end
+
   it 'returns input unaltered if it does not match' do
     input = 'hello'
 
@@ -173,41 +206,4 @@ RSpec.describe CodeSnippetFilter do
     expect(output).to include(expected_events_url)
     expect(output).to include(expected_answer_url)
   end
-end
-
-def example_source_file
-  <<~HEREDOC
-    require 'dotenv/load'
-    require 'nexmo'
-
-    NEXMO_API_KEY = ENV['NEXMO_API_KEY']
-    NEXMO_API_SECRET = ENV['NEXMO_API_SECRET']
-    NEXMO_APPLICATION_ID = ENV['NEXMO_APPLICATION_ID']
-    NEXMO_APPLICATION_PRIVATE_KEY_PATH = ENV['NEXMO_APPLICATION_PRIVATE_KEY_PATH']
-    NEXMO_NUMBER = ENV['NEXMO_NUMBER']
-    TO_NUMBER = ENV['TO_NUMBER']
-
-    client = Nexmo::Client.new(
-      api_key: NEXMO_API_KEY,
-      api_secret: NEXMO_API_SECRET,
-      application_id: NEXMO_APPLICATION_ID,
-      private_key: File.read(NEXMO_APPLICATION_PRIVATE_KEY_PATH)
-    )
-
-    response = client.calls.create(
-      to: [{
-        type: 'phone',
-        number: TO_NUMBER
-      }],
-      from: {
-        type: 'phone',
-        number: NEXMO_NUMBER
-      },
-      answer_url: [
-        'https://developer.nexmo.com/ncco/tts.json'
-      ]
-    )
-
-    puts response.inspect
-  HEREDOC
 end
