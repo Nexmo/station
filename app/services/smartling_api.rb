6e93c6c800a5a1a986437e1a3b5f32f8516716b8
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2020-03-18 13:10:24 +0100

    [DEVX-1862] Fix file download.

--- app/services/smartling_api.rb
+++ app/services/smartling_api.rb
@@ -19,13 +19,24 @@ class SmartlingAPI
       @client.upload(
         file.path,
         file_uri,
-        'markdown',
+        file_type(filename),
         'smartling.markdown_code_notranslate': true
       )
     end
   ensure
     file.close
     file.unlink
+  end
+
+  def file_type(filename)
+    case Pathname.new(filename).extname
+    when '.md', '.markdown'
+      'markdown'
+    when '.yml', '.yaml'
+      'yaml'
+    else
+      raise 'Unsupported file type.'
+    end
   end
 
   def last_modified(filename:, locale:)
@@ -39,8 +50,9 @@ class SmartlingAPI
       response = @client.download_translated(file_uri, locale, retrievalType: type)
 
       locale = locale_without_region(locale.to_s)
-      FileUtils.mkdir_p(storage_folder(locale, filename)) unless File.exist?(storage_folder(locale, filename))
-      File.open("#{Rails.configuration.docs_base_path}/_documentation/#{locale}/#{file_uri}", 'w+') do |file|
+      folder = storage_folder(filename, locale)
+      FileUtils.mkdir_p(folder) unless File.exist?(folder)
+      File.open(file_path(filename, locale), 'w+') do |file|
         file.write(Nexmo::Markdown::I18n::SmartlingConverterFilter.call(response))
       end
     end
@@ -48,13 +60,29 @@ class SmartlingAPI
 
   private
 
+  def file_path(filename, locale)
+    folder = storage_folder(filename, locale)
+    file_name = if filename.starts_with? '_documentation'
+                  Pathname.new(file_uri(filename)).basename.to_s
+                elsif filename.starts_with? 'config/locales'
+                  "#{locale}#{Pathname.new(file_uri(filename)).extname}"
+                end
+    "#{folder}/#{file_name}"
+  end
+
   def locale_without_region(locale)
     ['zh-CN', 'cn'].include?(locale) ? :cn : :en
   end
 
-  def storage_folder(locale, filename)
-    dir_path = Pathname.new(file_uri(filename)).dirname.to_s
-    "#{Rails.configuration.docs_base_path}/_documentation/#{locale}/#{dir_path}/"
+  def storage_folder(filename, locale)
+    if filename.starts_with? '_documentation'
+      dir_path = Pathname.new(file_uri(filename)).dirname.to_s
+      "#{Rails.configuration.docs_base_path}/_documentation/#{locale}/#{dir_path}"
+    elsif filename.starts_with? 'config/locales'
+      Pathname.new(file_uri(filename)).dirname.to_s
+    else
+      raise 'Unexpected file path'
+    end
   end
 
   def file_uri(filename)
