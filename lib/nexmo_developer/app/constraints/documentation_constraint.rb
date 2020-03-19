fa9a13f17ed887dcc94e42372b31849a1c397862
benhayehudi <bengreenberg@gmail.com>
2020-03-19 13:45:59 +0200

    move tasks to inside rails app structure and modify app.json

--- lib/nexmo_developer/app/constraints/documentation_constraint.rb
+++ lib/nexmo_developer/app/constraints/documentation_constraint.rb
@@ -3,11 +3,17 @@ class DocumentationConstraint
     Nexmo::Markdown::CodeLanguage.route_constraint.merge(product_with_parent)
   end
 
+  def self.products_for_routes
+    product_with_parent_list
+  end
+
   def self.product_with_parent
-    { product: Regexp.new(product_with_parent_list.compact.join('|')) }
+    { product: Regexp.new(product_with_parent_list.compact.reverse.join('|')) }
   end
 
   def self.product_with_parent_list
+    raise ArgumentError, "The 'product' key in config/products.yml must be a list with at least one entry." if config.fetch('products', []).size.zero?
+
     @product_with_parent_list ||= config.fetch('products', [])
   end
 
