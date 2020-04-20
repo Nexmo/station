3535973a19ead09f279c5a44eaa1f23fba34dedd
Fabian Rodriguez <fabianrbz@gmail.com>
2020-04-20 16:51:12 +0200

    [DEVX-2029] Remove DotNet. (#2704)

--- app/presenters/language.rb
+++
@@ -1,8 +0,0 @@
-class Language
-  def self.all
-    @all ||= begin
-               excluded_languages = ['csharp', 'javascript', 'kotlin', 'android', 'swift', 'objective_c']
-               Nexmo::Markdown::CodeLanguage.languages.reject { |l| excluded_languages.include?(l.key) }
-             end
-  end
-end
