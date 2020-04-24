fda3ce32b80da8d014a25ae0291296bf9b9aa705
benhayehudi <bengreenberg@gmail.com>
2020-04-24 11:39:09 +0300

    refactor topnav and add spec

--- lib/nexmo_developer/app/presenters/topnav.rb
+++ lib/nexmo_developer/app/presenters/topnav.rb
@@ -7,7 +7,7 @@ class Topnav
   end
 
   def navbar_items_from_config(config)
-    config = YAML.safe_load(File.open(config))
+    config = YAML.safe_load(open_config(config))
     @items ||= config.map do |name, url_path|
       {
         name: I18n.t("layouts.partials.header.#{name.downcase}"),
@@ -17,10 +17,18 @@ class Topnav
     end
   end
 
+  def open_config(config)
+    File.open(config)
+  end
+
+  def config_exist?(path)
+    File.exist?(path)
+  end
+
   private
 
   def after_initialize!
-    raise 'You must provide a config/top_navigation.yml file in your documentation path.' unless File.exist?("#{Rails.configuration.docs_base_path}/config/top_navigation.yml")
+    raise 'You must provide a config/top_navigation.yml file in your documentation path.' unless config_exist?("#{Rails.configuration.docs_base_path}/config/top_navigation.yml")
 
     @items = navbar_items_from_config("#{Rails.configuration.docs_base_path}/config/top_navigation.yml")
   end
