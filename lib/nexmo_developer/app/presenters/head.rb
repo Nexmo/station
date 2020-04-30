1c97689112c8d38dd6c75d54b30ba913ebd57b0f
benhayehudi <bengreenberg@gmail.com>
2020-04-30 18:29:43 +0300

    raise exceptions if required meta tags are missing

--- lib/nexmo_developer/app/presenters/head.rb
+++ lib/nexmo_developer/app/presenters/head.rb
@@ -9,11 +9,11 @@ class Head
   def header_from_config(config)
     config = YAML.safe_load(open_config(config))
     {
-      title: config['title'],
-      description: config['description'],
+      title: set_title(config),
+      description: set_description(config),
       google_site_verification: config['google-site-verification'],
       application_name: config['application-name'],
-      og_image: config['og-image'],
+      og_image: set_og_image(config),
       og_image_width: config['og-image-width'] || 835,
       og_image_height: config['og-image-height'] || 437
     }
@@ -33,5 +33,23 @@ class Head
     raise 'You must provide a config/business_info.yml file in your documentation path.' unless config_exist?("#{Rails.configuration.docs_base_path}/config/header_meta.yml")
 
     @items = header_from_config("#{Rails.configuration.docs_base_path}/config/header_meta.yml")
+  end
+
+  def set_title(config)
+    raise "You must provide a 'title' parameter in header_meta.yml" if config['title'].blank?
+
+    config['title']
+  end
+
+  def set_description(config)
+    raise "You must provide an 'description' parameter in header_meta.yml" if config['description'].blank?
+
+    config['description']
+  end
+
+  def set_og_image(config)
+    raise "You must provide an 'og-image' parameter in header_meta.yml" if config['og-image'].blank?
+
+    config['og-image']    
   end
 end
