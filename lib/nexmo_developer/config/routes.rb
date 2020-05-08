6127e62a90141dd2af536efc478e7b8938294a55
benhayehudi <bengreenberg@gmail.com>
2020-05-08 14:11:47 +0300

    remove static route for tools

--- lib/nexmo_developer/config/routes.rb
+++ lib/nexmo_developer/config/routes.rb
@@ -62,7 +62,6 @@ Rails.application.routes.draw do
   get '/community/slack', to: 'slack#join'
   post '/community/slack', to: 'slack#invite'
 
-  get '/tools', to: 'static#tools'
   get '/community/past-events', to: 'static#past_events'
 
   get '/feeds/events', to: 'feeds#events'
