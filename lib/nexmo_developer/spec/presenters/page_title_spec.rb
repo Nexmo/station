3d7c95d9e1c6aebd9130f89ae7e3220794f2b2de
benhayehudi <bengreenberg@gmail.com>
2020-07-01 11:46:40 +0300

    provide sample default in spec

--- lib/nexmo_developer/spec/presenters/page_title_spec.rb
+++ lib/nexmo_developer/spec/presenters/page_title_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PageTitle, type: :model do
   let(:product) { nil }
 
-  subject { described_class.new(product, title) }
+  subject { described_class.new(product, title, 'Vonage API Developer') }
 
   describe '#title' do
     context 'with title' do
