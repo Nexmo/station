c62c232d844b83aa4669857fa283b098c339ddff
benhayehudi <bengreenberg@gmail.com>
2020-07-06 13:23:54 +0300

    normalize page titles
    refactor to be config driven
    
    push up sample normalized words config
    
    add new config to fixtures
    
    make config not required, memoize and provide default
    
    refactor to use name field in products config for page title
    
    correct exception message
    
    consistent spacing
    
    remove instructions and sample config
    
    update spec
    
    memoization of config_path
    
    remove unneeded config file
    
    multi-line memoization
    
    rubocop

--- lib/nexmo_developer/app/presenters/page_title.rb
+++ lib/nexmo_developer/app/presenters/page_title.rb
@@ -6,11 +6,26 @@ class PageTitle
 
   def title
     if @product && @document_title
-      "#{@product.titleize} > #{@document_title} | #{default_title}"
+      "#{product_title_from_config(@product)} > #{@document_title} | #{default_title}"
     elsif @document_title
       "#{@document_title} | #{default_title}"
     else
       default_title
+    end
+  end
+
+  def product_title_from_config(product)
+    config = load_config
+    item = config['products'].select { |config_product| config_product['path'] == product }[0]
+    item['name']
+  end
+
+  def load_config
+    @load_config ||= begin
+      path ||= "#{Rails.configuration.docs_base_path}/config/products.yml"
+      raise 'You must provide a config/products.yml file in your documentation path.' unless File.exist?(path)
+
+      YAML.safe_load(File.open(path))
     end
   end
 
