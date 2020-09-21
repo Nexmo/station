f55989d5962e4904aa7d09354510b9a71a59912f
bencgreenberg <bengreenberg@gmail.com>
2020-09-21 11:50:32 +0300

    DRY up spec

--- lib/nexmo_developer/spec/services/translator/translator_coordinator_spec.rb
+++ lib/nexmo_developer/spec/services/translator/translator_coordinator_spec.rb
@@ -1,30 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Translator::TranslatorCoordinator do
+  let(:paths) { ['voice/voice-api/guides/numbers.md', 'messages/external-accounts/overview.md'] }
+  subject { described_class.new(paths: paths) }
+
   it 'accepts an array of docs paths for initialization' do
-    paths = ['voice/voice-api/guides/numbers.md', 'messages/external-accounts/overview.md']
-
-    coordinator = described_class.new(paths: paths)
-
-    expect(coordinator.paths).to be_an(Array)
+    expect(subject.paths).to be_an(Array)
   end
 
   describe '#requests' do
     it 'returns an Array of Translator::TranslationRequests instances' do
-      paths = ['voice/voice-api/guides/numbers.md', 'messages/external-accounts/overview.md']
-      coordinator = described_class.new(paths: paths)
-      requests = coordinator.requests
-
-      expect(requests).to all(be_a(Translator::TranslationRequest))
+      expect(subject.requests).to all(be_a(Translator::TranslationRequest))
     end
   end
 
   describe '#jobs' do
     it 'returns the requests grouped by translation frequency in ascending order' do
-      paths = ['voice/voice-api/guides/numbers.md', 'messages/external-accounts/overview.md']
-      coordinator = described_class.new(paths: paths)
-      jobs = coordinator.jobs
-      expect(jobs.keys).to eql(jobs.keys.sort)
+      expect(subject.jobs.keys).to eql(subject.jobs.keys.sort)
     end
   end
 end
