227bd3826e72d7bb3707ca7f901d83c0c71ee176
bencgreenberg <bengreenberg@gmail.com>
2020-09-22 17:46:43 +0300

    rubocop

--- lib/nexmo_developer/spec/services/translator/smartling/api_requests_generator_spec.rb
+++ lib/nexmo_developer/spec/services/translator/smartling/api_requests_generator_spec.rb
@@ -43,13 +43,13 @@ RSpec.describe Translator::Smartling::ApiRequestsGenerator do
         expect(Net::HTTP::Post).to receive(:new).and_return(mock_net_http_post)
 
         response = Net::HTTPSuccess.new(1.1, 200, 'OK')
-        expect(response).to receive(:body) .and_return('{"data": {"translationJobUuid": "uuid-translation"}}')
+        expect(response).to receive(:body).and_return('{"data": {"translationJobUuid": "uuid-translation"}}')
 
         allow(mock_net_http_post).to receive(:body=)
-                                         .with("{\"jobName\":\"ADP Translation Job: Tue, 22 Sep 2020 10:47:58 UTC +00:00\",\"targetLocaleIds\":[\"en\",\"ja\"],\"dueDate\":\"Thu, 24 Sep 2020 12:37:40 UTC +00:00\"}")
+          .with('{"jobName":"ADP Translation Job: Tue, 22 Sep 2020 10:47:58 UTC +00:00","targetLocaleIds":["en","ja"],"dueDate":"Thu, 24 Sep 2020 12:37:40 UTC +00:00"}')
 
         expect(mock_net_http).to receive(:request).with(mock_net_http_post).and_return(response)
-        expect(subject.create).to eq("uuid-translation")
+        expect(subject.create).to eq('uuid-translation')
       end
 
       it 'returns a translation job UUID' do
