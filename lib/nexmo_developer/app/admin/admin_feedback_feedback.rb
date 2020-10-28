33822d936162f2aef05a41b0bff9491cd8c8ddeb
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2020-10-28 14:47:52 +0100

    [Feedback] Add support for the new Feedback in the admin pages.

--- lib/nexmo_developer/app/admin/admin_feedback_feedback.rb
+++ lib/nexmo_developer/app/admin/admin_feedback_feedback.rb
@@ -26,4 +26,29 @@ ActiveAdmin.register ::Feedback::Feedback, as: 'Feedback' do
     end
     actions
   end
+
+  show do
+    attributes_table do
+      row :sentiment
+      row :resource
+      row :owner
+      row :owner_type
+      row :ip
+      row :comment
+      row :created_at
+      row :updated_at
+      row :code_language
+      row :code_language_selected_whilst_on_page
+      row :code_language_set_by_url
+      row :resolved
+      row :path
+      row :steps do
+        if feedback.config
+          render 'steps', { feedback: feedback }
+        end
+      end
+    end
+
+    active_admin_comments
+  end
 end
