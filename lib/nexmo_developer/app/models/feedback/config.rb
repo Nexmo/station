30247c3e9256a8dec7039c580f96e1af247a6bd9
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2020-10-30 11:06:41 +0100

    Make rubocop happy.

--- lib/nexmo_developer/app/models/feedback/config.rb
+++ lib/nexmo_developer/app/models/feedback/config.rb
@@ -1,9 +1,9 @@
 module Feedback
   class Config < ApplicationRecord
-    has_many :feedbacks, inverse_of: :config
+    has_many :feedbacks, inverse_of: :config, dependent: :nullify
 
     def self.find_or_create_config(config)
-      last_config = self.last
+      last_config = last
       if last_config && last_config.attributes.slice('title', 'paths') == config
         return last_config
       end
