5754565980437929d39253cd5859f19aa8c1d7d6
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2020-12-14 20:05:27 -0300

    Fix meta description tag.
    Pass `@frontmatter` to the presenter so the keys `meta_description` or
    `description` are used.

--- lib/nexmo_developer/spec/views/layouts/partials/_head.html.erb_spec.rb
+++ lib/nexmo_developer/spec/views/layouts/partials/_head.html.erb_spec.rb
@@ -14,5 +14,22 @@ RSpec.describe 'head', type: :view do
       expect(Capybara::Node::Simple.new(subject))
         .to have_css('meta[name="description"][content="A Description"]', visible: false)
     end
+
+    context 'with a @frontmatter' do
+      let(:frontmatter) do
+        YAML.safe_load(
+          File.read(
+            "#{Rails.configuration.docs_base_path}/_documentation/en/messaging/sms/overview.md"
+          )
+        )
+      end
+
+      before { assign(:frontmatter, frontmatter) }
+
+      it 'renders the meta_description from the frontmatter' do
+        expect(Capybara::Node::Simple.new(subject))
+          .to have_css('meta[name="description"][content="This documentation provides information on using the Nexmo SMS API for sending and receiving text messages."]', visible: false)
+      end
+    end
   end
 end
