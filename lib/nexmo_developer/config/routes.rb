8d2524204b80aa1d0332707f618826e71a920fc8
bencgreenberg <bengreenberg@gmail.com>
2021-01-13 08:10:19 +0200

    remove auto config added route

--- lib/nexmo_developer/config/routes.rb
+++ lib/nexmo_developer/config/routes.rb
@@ -1,7 +1,6 @@
 return unless ENV['DOCS_BASE_PATH']
 
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   devise_for :users, ActiveAdmin::Devise.config
