5018849ea58179d296bb99f97a7309556a326076
bencgreenberg <bengreenberg@gmail.com>
2021-01-21 10:45:42 +0200

    account for heroku prod

--- lib/nexmo_developer/app/presenters/improve_page_presenter.rb
+++ lib/nexmo_developer/app/presenters/improve_page_presenter.rb
@@ -7,13 +7,16 @@ class ImprovePagePresenter
     @github_url ||= "https://github.com/#{docs_repo}/blob/#{ENV.fetch('branch', 'master')}/#{path_to_url}"
   end
 
-  private
-
-  def path_to_url
-    @document_path&.gsub("#{Rails.configuration.docs_base_path}/", '')
+  def docs_repo
+    @docs_repo ||= begin
+      path_to_url.start_with?('lib/nexmo_developer/app/') ? 'nexmo/station' : YAML.safe_load(File.open("#{Rails.configuration.docs_base_path}/config/business_info.yml"))['docs_repo']
+    end
   end
 
-  def docs_repo
-    @docs_repo ||= YAML.safe_load(File.open("#{Rails.configuration.docs_base_path}/config/business_info.yml"))['docs_repo']
+  def path_to_url
+    @path_to_url ||= begin
+      @document_path&.gsub!("#{Rails.configuration.docs_base_path}/", '')
+      @document_path.start_with?('app/views') ? @document_path.prepend('lib/nexmo_developer/') : @document_path
+    end
   end
 end
