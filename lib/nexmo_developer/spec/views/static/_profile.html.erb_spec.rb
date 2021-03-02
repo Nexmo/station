427bcb4e1cf8da0a0502b87588517fa95b5123d8
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2021-03-02 11:53:03 +0100

    Point gravatar to a fork.
    The upstream repo is no longer maintained and doesn't support ruby 3.

---
+++ lib/nexmo_developer/spec/views/static/_profile.html.erb_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'static/profile.html.erb' do
+  it 'renders the profile' do
+    render partial: 'static/profile.html.erb', locals: { twitter: 'vonagedevs', name: 'John Doe', email: 'john.doe@vonage.com' }
+
+    view = Capybara::Node::Simple.new(rendered)
+
+    expect(view).to have_css('h3', text: 'John Doe')
+    expect(view).to have_css('p', text: 'Developer Advocate')
+    expect(view).to have_link('@vonagedevs', href: 'https://twitter.com/vonagedevs')
+  end
+end
