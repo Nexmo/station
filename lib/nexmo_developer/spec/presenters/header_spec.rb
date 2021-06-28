a4cf8beb3265af2d1618a28809ea3f06aefe53c2
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2021-06-28 16:28:15 +0200

    Fix specs.

--- lib/nexmo_developer/spec/presenters/header_spec.rb
+++ lib/nexmo_developer/spec/presenters/header_spec.rb
@@ -13,57 +13,6 @@ RSpec.describe Header do
     end
   end
 
-  describe '#logo_path' do
-    subject { described_class.new.logo_path }
-
-    it { expect(subject).to eq('/images/logos/sample-logo.png') }
-  end
-
-  describe '#small_logo_path' do
-    subject { described_class.new.small_logo_path }
-
-    it { expect(subject).to eq('/images/logos/Vonage-lettermark.svg') }
-  end
-
-  describe '#logo_alt' do
-    subject { described_class.new.logo_alt }
-
-    it { expect(subject).to eq('Sample Alt') }
-  end
-
-  describe '#name' do
-    subject { described_class.new.name }
-
-    it { expect(subject).to eq('Sample Name') }
-  end
-
-  describe '#subtitle' do
-    subject { described_class.new.subtitle }
-
-    it { expect(subject).to eq('Sample Subtitle') }
-  end
-
-  describe '#hiring_link?' do
-    subject { described_class.new.hiring_link? }
-
-    it { expect(subject).to eq(true) }
-
-    context 'when hiring.display is missing' do
-      let(:yaml) do
-        <<~HEREDOC
-          header:
-            hiring:
-        HEREDOC
-      end
-
-      it 'raises' do
-        allow(File).to receive(:open).and_call_original
-        allow(File).to receive(:open).and_return(yaml)
-        expect { subject }.to raise_error(RuntimeError, 'You must provide a true or false value for the hiring display parameter inside the header section of the config/business_info.yml file')
-      end
-    end
-  end
-
   describe '#sign_up_path' do
     subject { described_class.new.sign_up_path }
 
