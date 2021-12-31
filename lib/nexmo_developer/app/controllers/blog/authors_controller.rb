1a1eea90e8c99f1ad583eb9e05dd455dfc7ee00d
marco ranieri <mr.marcoranieri@gmail.com>
2021-12-31 19:59:12 +0000

    REFACT: refactored AuthorsController#SHOW (model & view)

--- lib/nexmo_developer/app/controllers/blog/authors_controller.rb
+++ lib/nexmo_developer/app/controllers/blog/authors_controller.rb
@@ -1,7 +1,8 @@
 class Blog::AuthorsController < Blog::MainController
   def show
-    # @author    = AuthorParser.fetch_author(params[:name])
-    @author    = { params[:name].to_sym => AuthorParser.fetch_author(params[:name]) }
-    @blogposts = AuthorParser.fetch_all_blogposts_from(params[:name])
+    data       = BlogpostParser.fetch_all
+    attributes = AuthorParser.fetch_author(params[:name])
+    
+    @author    = Blog::Author.new(attributes).build_all_blogposts_from_author(data)
   end
 end
--- lib/nexmo_developer/app/models/blog/author.rb
+++ lib/nexmo_developer/app/models/blog/author.rb
@@ -2,32 +2,52 @@
 class Blog::Author
   attr_reader :name, :title, :bio, :short_name, :email, :image_url,
               :author, :alumni, :team, :hidden, :spotlight, :noteworthy,
-              :website_url, :twitter, :linkedin_url, :github_url, :youtube_url, :facebook_url
+              :website_url, :twitter, :linkedin_url, :github_url, :youtube_url, 
+              :facebook_url, :stackoverflow_url, :twitch_url, :blogposts, :url
 
-  def initialize(attributes)
-    attributes.each {|k, v| self.instance_variable_set("@#{k}", v)} 
+  def initialize(attributes = {})
+    @name       = attributes['name']
+    @title      = attributes['title']
+    @bio        = attributes['bio']
+    @short_name = attributes['short_name']
+    @email      = attributes['email']
+    @image_url  = attributes['image_url']      || ''
+    
+    @author     = attributes['author']         || ''
+    @alumni     = attributes['alumni']         || false
+    @team       = attributes['team']           || false
+    @hidden     = attributes['hidden']         || false
+    @spotlight  = attributes['spotlight']      || false
+    @noteworthy = attributes['noteworthy']     || ''
+    
+    @website_url  = attributes['website_url']  || ''
+    @twitter      = attributes['twitter']      || ''
+    @linkedin_url = attributes['linkedin_url'] || ''
+    @github_url   = attributes['github_url']   || ''
+    @youtube_url  = attributes['youtube_url']  || ''
+    @facebook_url = attributes['facebook_url'] || ''
+    @twitch_url   = attributes['twitch_url']   || ''
+    @stackoverflow_url = attributes['stackoverflow_url'] || ''
 
-    # @name       = attributes['name']
-    # @title      = attributes['title']
-    # @bio        = attributes['bio']
-    # @short_name = attributes['short_name']
-    # @email      = attributes['email']
-    # @image_url  = attributes['image_url']
-    
-    # @author     = attributes['author'] || ''
-    # @alumni     = attributes['alumni'] || false
-    # @team       = attributes['team'] || false
-    # @hidden     = attributes['hidden'] || false
-    # @spotlight  = attributes['spotlight'] || false
-    # @noteworthy = attributes['noteworthy'] || ''
-    
-    # @website_url  = attributes['website_url'] || ''
-    # @twitter      = attributes['twitter'] || ''
-    # @linkedin_url = attributes['linkedin_url'] || ''
-    # @github_url   = attributes['github_url'] || ''
-    # @youtube_url  = attributes['youtube_url'] || ''
-    # @facebook_url = attributes['facebook_url'] || ''
+    @blogposts = []
+    @url       = build_url
   end
 
+  def build_all_blogposts_from_author(blogposts_json)
+    # TODO: How to handle authors with no Json info? (assay-and-roy)
+    @blogposts = blogposts_json.select do |b| 
+      if b['author'] && b['author']['short_name'].present?
+        b['author']['short_name'].downcase == @short_name.downcase
+      end
+    end.map { |b| Blog::Blogpost.new b }
+
+    self
+  end
+
+  def build_url
+    @url = image_url.include?('gravatar') ? 
+             image_url : 
+               "#{Blog::Blogpost::CLOUDFRONT_BLOG_URL}authors/#{image_url.gsub('/content/images/','')}"    
+  end
 end
   
