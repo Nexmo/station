c7cbc654bff0b6893a3d7fcbb9a37bac966462dc
marco ranieri <mr.marcoranieri@gmail.com>
2022-02-08 16:58:11 +0000

    ADD: Controllers Test (Authors - Blogposts - Categories) + added gem 'rails-controller-testing'

---
+++ lib/nexmo_developer/spec/controllers/blog/authors_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe Blog::AuthorsController, type: :controller do
+  # describe 'GET index' do
+  #   it 'returns a 200 HTTP code' do
+  #     get :index
+  #     expect(response).to have_http_status(:ok)
+  #   end
+
+  #   it 'renders the index template' do
+  #     get :index
+  #     expect(response).to render_template(:index)
+  #   end
+  # end
+
+  describe 'GET show' do
+    context "with author that exists" do
+      let(:author) { 'abdul-ajetunmobi'}
+
+      it 'returns a 200 HTTP code' do
+        get :show, :params => { name: author }
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'renders the show template' do
+        get :show, :params => { name: author }
+        expect(response).to render_template(:show)
+      end
+    end
+
+    context "with author that does not exist" do
+      let(:author) { 'joe-bloggs'}
+
+      # it 'returns a 404 HTTP code' do
+      #   get :show, :params => { name: author }
+      #   expect(response).to have_http_status(404)
+      # end
+
+      # it 'renders the custom 404 template' # TODO: once we have the custom 404 page
+    end
+  end
+end
