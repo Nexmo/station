0b463d8308150ff2a321cfa21a591936f810181b
marco ranieri <mr.marcoranieri@gmail.com>
2022-05-09 16:18:00 +0200

    APIDOC-177: Rubocop

--- lib/nexmo_developer/config/initializers/content_security_policy.rb
+++ lib/nexmo_developer/config/initializers/content_security_policy.rb
@@ -5,17 +5,17 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+  #   policy.default_src :self, :https
+  #   policy.font_src    :self, :https, :data
+  #   policy.img_src     :self, :https, :data
+  #   policy.object_src  :none
+  #   policy.script_src  :self, :https
+  #   policy.style_src   :self, :https
 
-  policy.frame_ancestors :self, "https://vids.vonage.com"
+  policy.frame_ancestors :self, 'https://vids.vonage.com'
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
+  #   # Specify URI for violation reports
+  #   # policy.report_uri "/csp-violation-report-endpoint"
 end
 
 # If you are using UJS then enable automatic nonce generation
