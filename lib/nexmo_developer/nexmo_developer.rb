bd0c55512c430313e80c440ecc4db9c0de46becd
Benjamin Aronov <benjamin@benyas.com>
2022-06-14 14:55:55 +0300

    remove all code associated with gravatar_image_tag gravatar

--- lib/nexmo_developer/nexmo_developer.rb
+++ lib/nexmo_developer/nexmo_developer.rb
@@ -1,12 +1,10 @@
 require_relative 'version'
-
 require 'rails/all'
 require 'activeadmin'
 require 'bootsnap'
 require 'bugsnag'
 require 'devise'
 require 'geocoder'
-require 'gravatar_image_tag'
 require 'greenhouse_io'
 require 'recaptcha'
 require 'split/dashboard'
