fdd69adfd7d8d91f4e20f858f5d3f491d4a3b4ad
Benjamin Aronov <benjamin@benyas.com>
2022-06-22 12:26:11 +0300

    deleted twitter link test for profile

--- lib/nexmo_developer/spec/views/static/_profile.html.erb_spec.rb
+++ lib/nexmo_developer/spec/views/static/_profile.html.erb_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'static/profile.html.erb' do
   it 'renders the profile' do
-    render partial: 'static/profile.html.erb', locals: { name: 'John Doe', email: 'john.doe@vonage.com', blog_profile_url: '' }
+    render partial: 'static/profile.html.erb', locals: { name: 'John Doe', email: 'john.doe@vonage.com', image_url: '' }
 
     view = Capybara::Node::Simple.new(rendered)
 
     expect(view).to have_css('h3', text: 'John Doe')
     expect(view).to have_css('p', text: 'Developer Advocate')
-    expect(view).to have_link('@vonagedevs', href: 'https://twitter.com/vonagedevs')
   end
 end
