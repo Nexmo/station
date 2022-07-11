c296e5a8e90ab5fda47f06ef9c0e044ba5b59f33
Benjamin Aronov <benjamin@benyas.com>
2022-07-11 18:14:10 +0300

    that's a nice short link

--- lib/nexmo_developer/spec/views/static/_profile.html.erb_spec.rb
+++ lib/nexmo_developer/spec/views/static/_profile.html.erb_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'static/profile.html.erb' do
+  let(:author) { Blog::Author.new({'name' => 'John Doe', 'title' => 'Developer Advocate', 'short_name' => 'john-doe'}) }
+
   it 'renders the profile' do
-    render partial: 'static/profile.html.erb', locals: { blog_profile_url: 'https://developer.vonage.com/blog', name: 'John Doe', email: 'john.doe@vonage.com', image_url: '' }
+    render partial: 'static/profile.html.erb', locals: { member: author }
 
     view = Capybara::Node::Simple.new(rendered)
 
@@ -10,6 +12,6 @@ RSpec.describe 'static/profile.html.erb' do
     expect(view).to have_css('p', text: 'Developer Advocate')
     expect(view).to have_css('svg')
     expect(view).to have_css('a', id: 'blog-profile-url')
-    expect(view).to have_link(href: 'https://developer.vonage.com/blog')
+    expect(view).to have_link(href: '/blog/authors/john-doe')
   end
 end
