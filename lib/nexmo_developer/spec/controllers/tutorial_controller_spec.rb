04cd1e19bf83f7cf72f2fdac9eb2f75d35eddb66
marco ranieri <mr.marcoranieri@gmail.com>
2022-07-21 17:28:21 +0200

    APIDOC-295: INTERIM fix test w/ params['product']

--- lib/nexmo_developer/spec/controllers/tutorial_controller_spec.rb
+++ lib/nexmo_developer/spec/controllers/tutorial_controller_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe TutorialController, type: :controller do
   it 'renders' do
-    get :list
+    get :list, params: { product: 'messaging/sms'}
 
     expect(response.status).to eq(200)
   end
 
   describe 'when a locale is present' do
     it 'redirects to the canonical url if locale is :en' do
-      get :list, params: { locale: 'en' }
+      get :list, params: { locale: 'en', product: '' }
 
       expect(response).to redirect_to('/tutorials')
     end
 
     it 'renders when locale is different from :en' do
-      get :list, params: { locale: 'cn' }
+      get :list, params: { locale: 'cn', product: 'messaging/sms' }
 
       expect(response.status).to eq(200)
     end
