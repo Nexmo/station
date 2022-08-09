3ec4992afb11e5b7f241d02f67861fe74a463e60
marco ranieri <mr.marcoranieri@gmail.com>
2022-08-09 15:03:15 +0200

    APIDOC-372: fix test - added fixtures (JSON)

--- lib/nexmo_developer/spec/services/blogpost_parser_spec.rb
+++ lib/nexmo_developer/spec/services/blogpost_parser_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe BlogpostParser do
 
   context 'self.fetch_all' do
     it 'can load blogposts/blogposts_info.json' do
-      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with('BLOG_PATH').and_return('/spec/fixtures/_blog')
 
       expect(BlogpostParser.fetch_all).to_not eq(nil)
     end
@@ -12,7 +12,7 @@ RSpec.describe BlogpostParser do
 
   context 'self.fetch_all_published' do
     it 'can load published blogposts from blogposts_info.json' do
-      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with('BLOG_PATH').and_return('/spec/fixtures/_blog')
 
       expect(BlogpostParser.fetch_all).to_not eq(BlogpostParser.fetch_all_published)
     end
