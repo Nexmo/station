352bd03dfcb8de5370b5f70ee192e2c355f2b223
marco ranieri <mr.marcoranieri@gmail.com>
2022-08-09 16:18:44 +0200

    APIDOC-372: fix test - removed allow(ENV)...

--- lib/nexmo_developer/spec/services/blogpost_parser_spec.rb
+++ lib/nexmo_developer/spec/services/blogpost_parser_spec.rb
@@ -3,18 +3,12 @@ require 'rails_helper'
 RSpec.describe BlogpostParser do
   context 'self.fetch_all' do
     it 'can load blogposts/blogposts_info.json' do
-      allow(ENV).to receive(:[]).and_call_original
-      allow(ENV).to receive(:[]).with('BLOG_PATH').and_return('/spec/fixtures/_blog')
-
       expect(BlogpostParser.fetch_all).to_not eq(nil)
     end
   end
 
   context 'self.fetch_all_published' do
     it 'can load published blogposts from blogposts_info.json' do
-      allow(ENV).to receive(:[]).and_call_original
-      allow(ENV).to receive(:[]).with('BLOG_PATH').and_return('/spec/fixtures/_blog')
-
       expect(BlogpostParser.fetch_all).to_not eq(BlogpostParser.fetch_all_published)
     end
   end
