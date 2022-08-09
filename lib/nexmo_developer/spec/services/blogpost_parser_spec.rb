fa83cbfec64ede65d60ed48468e44ab6fcf8aa0d
marco ranieri <mr.marcoranieri@gmail.com>
2022-08-09 13:21:39 +0200

    APIDOC-372: fix test using .and_call_original

--- lib/nexmo_developer/spec/services/blogpost_parser_spec.rb
+++ lib/nexmo_developer/spec/services/blogpost_parser_spec.rb
@@ -4,12 +4,16 @@ RSpec.describe BlogpostParser do
 
   context 'self.fetch_all' do
     it 'can load blogposts/blogposts_info.json' do
+      allow(ENV).to receive(:[]).and_call_original
+
       expect(BlogpostParser.fetch_all).to_not eq(nil)
     end
   end
 
   context 'self.fetch_all_published' do
     it 'can load published blogposts from blogposts_info.json' do
+      allow(ENV).to receive(:[]).and_call_original
+
       expect(BlogpostParser.fetch_all).to_not eq(BlogpostParser.fetch_all_published)
     end
   end
