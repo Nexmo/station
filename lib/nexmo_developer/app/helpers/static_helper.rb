f7ceb11fa63f91fadccc567638a10c9524a49809
lggg123 <machinelearner2334@gmail.com>
2022-08-19 13:37:54 -0700

    APIDOC-253: fixed rubocop errors

--- lib/nexmo_developer/app/helpers/static_helper.rb
+++ lib/nexmo_developer/app/helpers/static_helper.rb
@@ -1,12 +1,14 @@
 module StaticHelper
-    # this is to refactor_keys in landing_page_documentation.html.erb
-    def refactor_keys(resource)
-        resource.keys.first.to_s.gsub(' ', '-')
-    end
-    def refactor_class
-        "Vlt-btn Vlt-btn--blue Vlt-btn--app Vlt-btn--small"
-    end
-    def first(resource)
-        resource.keys.first
-    end
+  # this is to refactor_keys in landing_page_documentation.html.erb
+  def refactor_keys(resource)
+    resource.keys.first.to_s.gsub(' ', '-')
+  end
+
+  def refactor_class
+    'Vlt-btn Vlt-btn--blue Vlt-btn--app Vlt-btn--small'
+  end
+
+  def first(resource)
+    resource.keys.first
+  end
 end
