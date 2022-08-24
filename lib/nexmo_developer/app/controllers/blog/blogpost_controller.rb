d0a83c6ab851d6cce2f564fc1fef0d0ab311ab26
lggg123 <machinelearner2334@gmail.com>
2022-08-24 10:37:35 -0700

    APIDOC-176: removed whitspace rubocop errors

--- lib/nexmo_developer/app/controllers/blog/blogpost_controller.rb
+++ lib/nexmo_developer/app/controllers/blog/blogpost_controller.rb
@@ -23,7 +23,6 @@ class Blog::BlogpostController < Blog::MainController
     @blogpost = Blog::Blogpost.build_blogpost_from_path(params[:blog_path], 'en')
 
     @blogButtonPath = "https://dashboard.nexmo.com/sign-up?icid=tryitfree_adpblog-#{params[:blog_path]}_nexmodashbdfreetrialsignup_banner"
- 
     redirect_to blog_path unless @blogpost.published
 
     data = BlogpostParser.fetch_all_published
